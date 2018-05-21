89d695f644b892ded42ed8bc49bb26d2e90260b2
Colin <owens106@purde.edu>
2018-05-21 17:45:20 -0400

    broke up the header, gold Story, and actual story in the gold story

---
+++ src/GoldMadness.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import './App.css';
+import Story from './Story';
+
+//import header from './header';
+
+class GoldMadness extends Component {
+  render() {
+    return (
+        <div className='GoldMadness'>
+                <div className="large-8 medium-12 columns article">
+                    <h2 className="article-title">Gold Madness - Fact or Fiction?</h2>
+                    <div className="avatar">
+                        <img src="http://www.zbrushcentral.com/attachment.php?attachmentid=376082" alt="author" />
+                        <div className="author-info">
+                            <p className="author-name">By Domri, son of Flug</p>
+                            <p className="date">on 28 April, 3018 of the Third Age</p>
+                         </div>
+                    </div>
+                    {React.createElement(Story)}
+                    <div className="article-links">
+                        <a className="article-link" href="#">
+                            <i className="fa fa-comments-o"></i>
+                            <span className="article-link-text">Comments</span>
+                        </a>
+                        <a className="article-link" href="#">
+                             <i className="fa fa-share"></i>
+                            <span className="article-link-text">Share Post</span>
+                        </a>
+                    </div>
+                </div>
+        </div>
+
+    )
+
+  }
+}
+
+export default GoldMadness
---
+++ src/driver.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import app from './App'
+ReactDOM.render(
+    app
+)
